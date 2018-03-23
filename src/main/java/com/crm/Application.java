62ce34db93c827025a9a9beb764f1149115eb4d4
TeqXe <1358582114@qq.com>
2018-03-23 16:45:29 +0800

    删除.idea StuManager.iml

--- src/main/java/com/crm/Application.java
+++ src/main/java/com/crm/Application.java
@@ -5,12 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpStatus;
 
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 @Import({ DynamicDataSourceConfig.class })
@@ -23,16 +19,5 @@ public class Application extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Application.class);
-	}
-
-	@Bean
-	public EmbeddedServletContainerCustomizer containerCustomizer() {
-
-		return (container -> {
-			ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
-			ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
-			ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
-			container.addErrorPages(error401Page, error404Page, error500Page);
-		});
 	}
 }
--- src/main/java/com/crm/config/FilterConfig.java
+++ src/main/java/com/crm/config/FilterConfig.java
@@ -5,9 +5,12 @@ import java.util.Map;
 
 import com.crm.common.xss.XssFilter;
 
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.filter.DelegatingFilterProxy;
 
 import javax.servlet.DispatcherType;
@@ -39,5 +42,16 @@ public class FilterConfig {
 		initParameters.put("allowMethods", "GET,POST,PUT,DELETE,OPTIONS");
 		registration.setInitParameters(initParameters);
 		return registration;
+	}
+
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+
+		return (container -> {
+			ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
+			ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+			ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
+			container.addErrorPages(error401Page, error404Page, error500Page);
+		});
 	}
 }
